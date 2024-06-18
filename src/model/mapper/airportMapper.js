53df51b63721a6f49ac6757dbccab4a3831d341d
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-18 19:14:44 +0200

    feat: Added Airport model mapper.

---
+++ src/model/mapper/airportMapper.js
@@ -0,0 +1,25 @@
+const AirportDTO = require('../dto/AirportDTO');
+
+function toDTO(airport) {
+  return new AirportDTO(
+    airport.id,
+    airport.name,
+    airport.iata,
+    airport.latitude,
+    airport.longitude,
+    airport.elevation,
+    airport.continent,
+    airport.country,
+    airport.region,
+    airport.municipality
+  );
+}
+
+function toDTOList(airports) {
+  return airports.map(airport => this.toDTO(airport));
+}
+
+module.exports = {
+  toDTO,
+  toDTOList,
+};;
