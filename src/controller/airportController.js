4040d204902bb0cd6ff9ee107952902ad1fc96f2
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-18 19:16:14 +0200

    feat: Added Airport controller.

---
+++ src/controller/airportController.js
@@ -0,0 +1,25 @@
+const airportService = require('../service/airportService');
+
+async function findAll(req, res) {
+  try {
+    const airports = await airportService.findAll();
+    res.json(airports);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+async function search(req, res) {
+  const { search } = req.query;
+  try {
+    const airports = await airportService.search(search);
+    res.json(airports);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+module.exports = {
+    findAll,
+    search,
+};
