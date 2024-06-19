c1ea85a07c1fce35db96cc613aa4033b16b4211e
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-19 19:17:57 +0200

    feat: Date validator.

---
+++ src/utility/dateValidator.js
@@ -0,0 +1,14 @@
+const isFutureDate = (date, throwError = false) => {
+    const now = new Date();
+    const isFuture = date > now;
+
+    if (!isFuture && throwError) {
+        throw new Error('Validation: Must be a future date');
+    }
+
+    return isFuture;
+};
+  
+  module.exports = {
+    isFutureDate,
+};
